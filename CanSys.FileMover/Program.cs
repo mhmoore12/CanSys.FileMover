fc7e1112fafff6e9d3f5e56354899f6d1a8ac9ef
mhmoore12 <mhmoore12@gmail.com>
2024-10-29 13:51:12 -0500

    Add project files.

---
+++ CanSys.FileMover/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CanSys.FileMover
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
